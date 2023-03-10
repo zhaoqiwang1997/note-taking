1cf73e6b2b3ed561a2432c398f667176a168ff32
Zhaoqi Wang <zwan0242@student.monash.edu>
2023-03-10 13:19:27 +1100

    Tag and Note model

--- notetaking/models.py
+++ notetaking/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from datetime import date
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
 class Note(models.Model):
     title = models.CharField(max_length=100, primary_key=True)
     date = models.DateField(default=date.today)
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
     content = models.TextField()
